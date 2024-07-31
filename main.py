55f05467a3f89db86c49730ff02b52a8c8b0f8c0
Aditya <lapratomo24@yahoo.com>
2024-07-31 13:49:09 +0200

    add function to deal 2 random cards

--- main.py
+++ main.py
@@ -18,10 +18,28 @@ logo = '''
 # The deck is unlimited in size. 
 # There are no jokers. 
 # The Jack/Queen/King all count as 10.
-# The the Ace can count as 11 or 1.
+# The Ace can count as 11 or 1.
 # Use the following list as the deck of cards:
-# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 # The cards in the list have equal probability of being drawn.
 # Cards are not removed from the deck as they are drawn.
 # The computer is the dealer
 
+import random
+
+def deal_card():
+    return random.choice(cards)
+
+user_cards = []
+computer_cards = []
+
+for card in range(2):
+    random_card = deal_card()
+    user_cards.append(random_card)
+    
+for card in range(2):
+    random_card = deal_card()
+    computer_cards.append(random_card)
+    
+print(user_cards)
+print(computer_cards)
